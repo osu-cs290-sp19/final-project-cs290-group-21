9c224572d625c35c3d0e686cbd78aad1e8fac516
Gregory Blood <bloodg@flip2.engr.oregonstate.edu>
2019-06-06 15:49:08 -0700

    Greg: 06/06 -- LMAO

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function save() {
 }
 function reset() {
     var hessCoin = 0;
-    var value = 10;
-    var perclick = 0;
+    var value = 1;
+    var perClick = 0;
     var money = 0;
     var gpua = [1,0,0,0,0,0,0];
     var infa = [0,0,0,0,0,0,0];
@@ -17,16 +17,16 @@ function reset() {
 var save = document.getElementById("save").addEventListener("click", function() {
     save();
 });
-var reset =ndocument.getElementById("reset").addEventListener("click", function() {
+var reset = document.getElementById("reset").addEventListener("click", function() {
     reset();
 });
 //-----------------------------GAME LOGIC-------------------------------
 //amount of coins
 var hessCoin = 0;
-//value of coin base is 10
-var value = 10;
+//value of coin base is 1
+var value = 1;
 //coins created per click
-var perclick = 0;
+var perClick = 1;
 //money total
 var money = 0;
 //arrays for things purchased------------------
@@ -43,7 +43,8 @@ var infp = [1,10,100,1000,10000,100000,1000000];
 
 //Makes new hesscoin
 function makeCoin() {
-    hessCoin += value * perclick;
+    hessCoin += value * perClick;
+    money += value;
 }
 //calculates new price after purch 15%
 function newPrice(price) {
@@ -53,7 +54,7 @@ function newPrice(price) {
     //type is array spot
 function buyGPU(type) {
     gpua[type]++;
-    perclick += gpup[type];
+    perClick += gpup[type];
     money -= gpuc[type];
     gpuc[type] *= 1.15;
 }
@@ -65,26 +66,50 @@ function buyINF(type) {
     hessCoin -= infc[type];
     infc[type] *= 1.15;
 }
-var mine = document.getElementById("clicker").addEventListener("click", function() {
+function valueCalc () {
+    value += infp[type];
+}
+var Game = {};
+//Get HTML
+window.onload = function () {
+    var hessAmount = document.getElementById("hessCoins");
+    var hessValue = document.getElementById("perClick");
+    var hessMoney = document.getElementById("money");
+    //Per Frame
+    function update() {
+        hessAmount.innerHTML = hessCoin;
+        hessValue.innerHTML = perClick;
+        hessMoney.innerHTML = money;
+        setTimeout(update, 100);
+    }
+    update();
+
+}
+
+Game.mine = document.getElementById("clicker").addEventListener("click", function() {
     makeCoin();
 });
+/*NEED HTML
+
+
 //--------------------------GPU-------------------------------
-var buyGPU0 = document.getElementById("gpu0").addEventListener("click", function() {
+Game.buyGPU0 = document.getElementById("gpu0").addEventListener("click", function() {
     if (gpuc[0] <= hessCoin)
         buyGPU(0);
 });
-var buyGPU1 = document.getElementById("gpu1").addEventListener("click", function() {
+Game.buyGPU1 = document.getElementById("gpu1").addEventListener("click", function() {
     if (gpuc[1] <= hessCoin)
         buyGPU(1);
 });
 //--------------------------INF-------------------------------
-var buyINF0 = document.getElementById("inf0").addEventListener("click", function() {
+Game.buyINF0 = document.getElementById("inf0").addEventListener("click", function() {
     if (infc[0] <= hessCoin)
         buyINF(0);
 });
+*/
 
 //Stats
-function totalperclick () {
+function totalperClick () {
     var total = 0;
     for (var i = 0; i < 7; i++) {
         total += gpua[i] * gpup[i];
